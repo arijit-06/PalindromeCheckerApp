ef6b25dd9f0421b5bcd4aa7297758d2755bb31df
arijit-06 <arijit270906@gmail.com>
2026-02-26 14:22:40 +0530

    Added Use Case - II

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -26,7 +26,37 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         
         System.out.println("WELCOME TO PALINDROME CHECKER APP MANAGEMENT SYSTEM");
-        System.out.println("version:1.0");
+        System.out.println("version:2.0");
         System.out.println("System instanced successful");
+
+        String Palindrome = "racecar";
+        String NotPalindrome = "notaracecar";
+
+        int i, n, flag;
+
+        flag = 0;
+        n = Palindrome.length();
+        for (i = 0; i < n/2; i++){
+            if(Palindrome.charAt(i) != Palindrome.charAt(n-i-1)) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)  System.out.println("The string " + Palindrome + " is a palindrome.");
+        else System.out.println("The string " + Palindrome + " is not a palindrome.");
+
+
+        flag = 0;
+        n = NotPalindrome.length();
+        for (i = 0; i < n/2; i++){
+            if(NotPalindrome.charAt(i) != NotPalindrome.charAt(n-i-1)) {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)  System.out.println("The string " + NotPalindrome + " is a palindrome.");
+        else System.out.println("The string " + NotPalindrome + " is not a palindrome.");
     }
 }
