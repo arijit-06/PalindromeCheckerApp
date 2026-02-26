4b629781c683ca56a5f4b61cc615537f894ac1c3
arijit-06 <arijit270906@gmail.com>
2026-02-26 15:22:33 +0530

    UC3-Reversed String Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -26,37 +26,19 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
         
         System.out.println("WELCOME TO PALINDROME CHECKER APP MANAGEMENT SYSTEM");
-        System.out.println("version:2.0");
+        System.out.println("version:3.0");
         System.out.println("System instanced successful");
+        System.out.println();
 
         String Palindrome = "racecar";
-        String NotPalindrome = "notaracecar";
+        String Reverse = "";
 
-        int i, n, flag;
+        int i;
 
-        flag = 0;
-        n = Palindrome.length();
-        for (i = 0; i < n/2; i++){
-            if(Palindrome.charAt(i) != Palindrome.charAt(n-i-1)) {
-                flag = 1;
-                break;
-            }
+        for(i = Palindrome.length()-1; i >= 0; i--){
+            Reverse += Palindrome.charAt(i);
         }
-
-        if (flag == 0)  System.out.println("The string " + Palindrome + " is a palindrome.");
+        if (Palindrome.equals(Reverse))  System.out.println("The string " + Palindrome + " is a palindrome.");
         else System.out.println("The string " + Palindrome + " is not a palindrome.");
-
-
-        flag = 0;
-        n = NotPalindrome.length();
-        for (i = 0; i < n/2; i++){
-            if(NotPalindrome.charAt(i) != NotPalindrome.charAt(n-i-1)) {
-                flag = 1;
-                break;
-            }
-        }
-
-        if (flag == 0)  System.out.println("The string " + NotPalindrome + " is a palindrome.");
-        else System.out.println("The string " + NotPalindrome + " is not a palindrome.");
     }
 }
